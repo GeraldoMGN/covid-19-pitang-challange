fa998ece416de0ee4391412142b59513f905c532
Geraldo Galvao <gmgn@cin.ufpe.br>
2021-05-04 00:49:27 -0300

    Error message on Schedule Form

--- frontend/src/pages/SchedulePage/components/ScheduleForm.jsx
+++ frontend/src/pages/SchedulePage/components/ScheduleForm.jsx
@@ -3,108 +3,126 @@ import {
 } from 'formik';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { addSchedule } from '../../../services/requests/scheduling';
 import ScheduleTimePicker from './ScheduleTimePicker';
 
-const ScheduleForm = () => (
-  <Formik
-    initialValues={{
-      name: '', dateOfBirth: null, vaccinationTime: '', vaccinationDate: null,
-    }}
-    validate={(values) => {
-      const errors = {};
+const ScheduleForm = ({ onSubmissionError }) => {
+  const history = useHistory();
 
-      if (!values.name) {
-        errors.name = 'Insira seu nome';
-      }
-      if (!values.dateOfBirth) {
-        errors.dateOfBirth = 'Insira sua data de nascimento';
-      }
-      if (!values.vaccinationTime) {
-        errors.vaccinationTime = 'Insira o horário de vacinação';
-      }
-      if (!values.vaccinationDate) {
-        errors.vaccinationDate = 'Insira a data de vacinação';
-      }
+  return (
+    <Formik
+      initialValues={{
+        name: '', dateOfBirth: null, vaccinationTime: '', vaccinationDate: null,
+      }}
+      validate={(values) => {
+        const errors = {};
 
-      return errors;
-    }}
-    onSubmit={async ({
-      name, dateOfBirth, vaccinationTime, vaccinationDate,
-    }) => {
-      const date = new Date(vaccinationDate);
-      const time = new Date(vaccinationTime);
-      date.setHours(time.getHours(), time.getMinutes());
-      await addSchedule(date, name, dateOfBirth);
-    }}
-  >
-    {({
-      values, errors, setFieldValue, isSubmitting, touched, setFieldTouched,
-    }) => {
-      const handleIsInvalidClass = (fieldName) => (errors[fieldName] && touched[fieldName]
-        ? 'is-invalid'
-        : '');
+        if (!values.name) {
+          errors.name = 'Insira seu nome';
+        }
+        if (!values.dateOfBirth) {
+          errors.dateOfBirth = 'Insira sua data de nascimento';
+        }
+        if (!values.vaccinationTime) {
+          errors.vaccinationTime = 'Insira o horário de vacinação';
+        }
+        if (!values.vaccinationDate) {
+          errors.vaccinationDate = 'Insira a data de vacinação';
+        }
 
-      return (
-        <Form>
-          <div className="row">
-            <div className="col-12 col-sm-8 mb-3">
-              <label htmlFor="name" className="form-label">Nome completo</label>
-              <Field
-                className={`form-control ${handleIsInvalidClass('name')}`}
-                type="name"
-                name="name"
-                id="name"
-                placeholder="Insira seu nome"
-              />
-              <ErrorMessage className="text-danger" name="name" component="div" />
+        return errors;
+      }}
+      onSubmit={async ({
+        name, dateOfBirth, vaccinationTime, vaccinationDate,
+      }) => {
+        const date = new Date(vaccinationDate);
+        const time = new Date(vaccinationTime);
+        date.setHours(time.getHours(), time.getMinutes());
+
+        const response = await addSchedule(date, name, dateOfBirth);
+
+        if (response === 'Vacinação agendada!') {
+          // TODO: User feedback on success
+          history.push('/');
+        } else {
+          onSubmissionError(response);
+        }
+      }}
+    >
+      {({
+        values, errors, setFieldValue, isSubmitting, touched, setFieldTouched,
+      }) => {
+        const handleIsInvalidClass = (fieldName) => (errors[fieldName] && touched[fieldName]
+          ? 'is-invalid'
+          : '');
+
+        return (
+          <Form>
+            <div className="row">
+              <div className="col-12 col-sm-8 mb-3">
+                <label htmlFor="name" className="form-label">Nome completo</label>
+                <Field
+                  className={`form-control ${handleIsInvalidClass('name')}`}
+                  type="name"
+                  name="name"
+                  id="name"
+                  placeholder="Insira seu nome"
+                />
+                <ErrorMessage className="text-danger" name="name" component="div" />
+              </div>
+              <div className="col-12 col-sm-4 mb-3">
+                <label htmlFor="dateOfBirth" className="form-label w-100">Data de Nascimento</label>
+                <DatePicker
+                  name="dateOfBirth"
+                  className={`form-control ${handleIsInvalidClass('dateOfBirth')}`}
+                  selected={values.dateOfBirth}
+                  onChange={(date) => setFieldValue('dateOfBirth', date)}
+                  dateFormat="dd/MM/yyyy"
+                  maxDate={(new Date()).getTime()}
+                  onBlur={() => setFieldTouched('dateOfBirth', true)}
+                  wrapperClassName="w-100"
+                />
+                <ErrorMessage className="text-danger" name="dateOfBirth" component="div" />
+              </div>
             </div>
-            <div className="col-12 col-sm-4 mb-3">
-              <label htmlFor="dateOfBirth" className="form-label w-100">Data de Nascimento</label>
-              <DatePicker
-                name="dateOfBirth"
-                className={`form-control ${handleIsInvalidClass('dateOfBirth')}`}
-                selected={values.dateOfBirth}
-                onChange={(date) => setFieldValue('dateOfBirth', date)}
-                dateFormat="dd/MM/yyyy"
-                maxDate={(new Date()).getTime()}
-                onBlur={() => setFieldTouched('dateOfBirth', true)}
-                wrapperClassName="w-100"
-              />
-              <ErrorMessage className="text-danger" name="dateOfBirth" component="div" />
+            <div className="row my-3">
+              <h5>Quando você quer ser vacinado?</h5>
             </div>
-          </div>
-          <div className="row my-3">
-            <h5>Quando você quer ser vacinado?</h5>
-          </div>
-          <div className="row">
-            <div className="col-12 col-sm-6 mb-3">
-              <label htmlFor="vaccinationTime" className="form-label w-100">Horário</label>
-              <ScheduleTimePicker className={handleIsInvalidClass('vaccinationTime')} />
-              <ErrorMessage className="text-danger" name="vaccinationTime" component="div" />
+            <div className="row">
+              <div className="col-12 col-sm-6 mb-3">
+                <label htmlFor="vaccinationTime" className="form-label w-100">Horário</label>
+                <ScheduleTimePicker className={handleIsInvalidClass('vaccinationTime')} />
+                <ErrorMessage className="text-danger" name="vaccinationTime" component="div" />
+              </div>
+              <div className="col-12 col-sm-6 mb-3">
+                <label htmlFor="vaccinationDate" className="form-label w-100">Data</label>
+                <DatePicker
+                  name="vaccinationDate"
+                  className={`form-control ${handleIsInvalidClass('vaccinationDate')}`}
+                  selected={values.vaccinationDate}
+                  onChange={(date) => setFieldValue('vaccinationDate', date)}
+                  dateFormat="dd/MM/yyyy"
+                  minDate={(new Date()).getTime()}
+                  onBlur={() => setFieldTouched('vaccinationDate', true)}
+                  wrapperClassName="w-100"
+                />
+                <ErrorMessage className="text-danger" name="vaccinationDate" component="div" />
+              </div>
             </div>
-            <div className="col-12 col-sm-6 mb-3">
-              <label htmlFor="vaccinationDate" className="form-label w-100">Data</label>
-              <DatePicker
-                name="vaccinationDate"
-                className={`form-control ${handleIsInvalidClass('vaccinationDate')}`}
-                selected={values.vaccinationDate}
-                onChange={(date) => setFieldValue('vaccinationDate', date)}
-                dateFormat="dd/MM/yyyy"
-                minDate={(new Date()).getTime()}
-                onBlur={() => setFieldTouched('vaccinationDate', true)}
-                wrapperClassName="w-100"
-              />
-              <ErrorMessage className="text-danger" name="vaccinationDate" component="div" />
-            </div>
-          </div>
-          <button type="submit" disabled={isSubmitting} className="btn btn-primary btn-lg w-100 my-3">
-            Agendar
-          </button>
-        </Form>
-      );
-    }}
-  </Formik>
-);
+            <button type="submit" disabled={isSubmitting} className="btn btn-primary btn-lg w-100 my-3">
+              Agendar
+            </button>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
+
+ScheduleForm.propTypes = {
+  onSubmissionError: PropTypes.func.isRequired,
+};
 
 export default ScheduleForm;
--- frontend/src/pages/SchedulePage/index.jsx
+++ frontend/src/pages/SchedulePage/index.jsx
@@ -1,19 +1,24 @@
+import { useState } from 'react';
 import ScheduleForm from './components/ScheduleForm';
 import { scheduleContainer } from './index.module.scss';
 
-const SchedulePage = () => (
-  <div className="container mt-5">
-    <div className={scheduleContainer}>
-      <h3>Agende sua vacinação</h3>
-      <p>
-        É necessário informar seu nome, data de nascimento, além do dia e
-        horário para o agendamento.
-      </p>
+const SchedulePage = () => {
+  const [errorMessage, setErrorMessage] = useState('');
+  return (
+    <div className="container mt-5">
+      <div className={scheduleContainer}>
+        <h3>Agende sua vacinação</h3>
+        <p>
+          É necessário informar seu nome, data de nascimento, além do dia e
+          horário para o agendamento.
+        </p>
+      </div>
+      <div className={scheduleContainer}>
+        <ScheduleForm onSubmissionError={setErrorMessage} />
+        <h5 className="text-danger">{errorMessage}</h5>
+      </div>
     </div>
-    <div className={scheduleContainer}>
-      <ScheduleForm />
-    </div>
-  </div>
-);
+  );
+};
 
 export default SchedulePage;
